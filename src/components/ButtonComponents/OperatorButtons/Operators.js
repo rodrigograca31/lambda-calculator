cf5a3d52975d362c3e0038587614eadc4933dc63
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-05 12:19:20 +0000

    Step 2 :heavy_check_mark:

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -6,12 +6,14 @@ import { operators } from "../../../data"
 //Import your array data to from the provided data file
 
 const Operators = () => {
-  // STEP 2 - add the imported data to state
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+	// STEP 2 - add the imported data to state
+	const [operatorState, setOperatorState] = useState(operators);
+	
+	return (
+		<div>
+			{/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-    </div>
-  );
+		</div>
+	);
 };
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -6,13 +6,14 @@ import { specials } from "../../../data"
 //Import your array data to from the provided data file
 
 const Specials = () => {
-  // STEP 2 - add the imported data to state
+	// STEP 2 - add the imported data to state
+	const [specialState, setSpecialState] = useState(specials);
 
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+	return (
+		<div>
+			{/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-    </div>
-  );
+		</div>
+	);
 };
